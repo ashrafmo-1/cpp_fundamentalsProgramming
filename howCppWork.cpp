dad254bc6d35435de83ac25c9b779f149c893a35
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-25 16:21:40 +0200

    first push

---
+++ howCppWork.cpp
@@ -0,0 +1,14 @@
+// preprosersor statment
+#include <iostream>
+
+int main() // intry point "function"
+	// cout => chracter output
+	// << "operatot" => "stream in searchian operator"
+	// "string" => text is an string
+{
+	std::cout << "hello line one\n";
+	std::cout << "hello line two\n";
+	std::cout << "hello line three\n";
+	std::cout << "std::cout\n";
+	return 0;
+}
