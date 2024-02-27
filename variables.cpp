b631838ec9384aa55ebfc477ee10b65d28a77357
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-27 12:10:56 +0200

    start varibales

---
+++ variables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    // variable declar
+    int heght = 190;
+    std::cout << "you heght is " << heght << "cm.\n";
+    std::cout << "before 3 monthes plus 3cm , your heght now is: " << heght + 3;
+}
