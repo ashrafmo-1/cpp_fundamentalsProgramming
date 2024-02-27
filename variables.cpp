c4e7c92b14691d90ff7fed2526bf6c26a9324fdb
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-27 14:44:59 +0200

    variables

--- variables.cpp
+++ variables.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 int main(){
+
+    using namespace std; // to not retype std::
     // variable declar
     int heght = 190;
-    std::cout << "you heght is " << heght << "cm.\n";
-    std::cout << "before 3 monthes plus 3cm , your heght now is: " << heght + 3;
+    cout << "you heght is " << heght << "cm.\n";
+    cout << "before 3 monthes plus 3cm , your heght now is: " << heght + 3;
 }
