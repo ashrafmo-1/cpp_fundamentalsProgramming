77e05448b6a38efe79e2836b2966d4939e499ecb
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-29 13:29:04 +0200

    projects/ calc age

---
+++ projects/ageCalculate.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // header project
+    cout << "****************************************\n";
+    cout << "***** welcome to my age calculator *****\n";
+    cout << "****************************************\n";
+
+    int yourAge;
+    cin >> yourAge;
+    cout << yourAge;
+
+    int age_days = yourAge * 365;
+    int age_hours = yourAge * age_days * 365;
+
+    cout << "age_days is: " << age_days << "days\n";
+    cout << "age_hours is: " << age_hours << "days\n";
+
+
+    return 0;
+}
