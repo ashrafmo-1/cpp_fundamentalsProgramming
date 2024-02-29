2353d24cc4a8e10c96d0f55de9f5c1e1fddcb1f0
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-29 07:05:00 +0200

    Constant Variable

---
+++ variables/Constant Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+using namespace std;
+#define name "ashraf"
+
+int main()
+{
+  const int weekTasks = 30;
+  int salary = 111000;
+//   weekTasks = 200;  //* err */
+  cout << weekTasks << "\n";
+  cout << salary << "\n";
+  cout << name << "\n";
+}
