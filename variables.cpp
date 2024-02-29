98fa514ed30dd795a35d7e6629f7477c492caf62
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-29 06:29:29 +0200

    Variables Advanced Knowledge

--- variables.cpp
+++ variables.cpp
@@ -12,10 +12,31 @@
 #include <iostream>
 
 int main(){
-
     using namespace std; // to not retype std::
+
+    cout <<  "\n//* example one */\n";
     // variable declar
     int heght = 190;
     cout << "you heght is " << heght << "cm.\n";
     cout << "before 3 monthes plus 3cm , your heght now is: " << heght + 3;
+
+    cout <<  "\n//* example two */\n";
+    int x = 1;
+    cout << x;
+
+    cout <<  "\n//* example three */\n";
+    int y, z, w;
+    y = 10;
+    z = 20;
+    w = 30;
+    cout << y + z + w;
+    cout <<  "\n//* example four */\n";
+        int a, b, c;
+    a = 10, b = 20, c = 30;
+    cout << a + b + c;
+    cout <<  "\n//* example five */\n";
+    int g, t, r;
+    g = t = r = 20;
+    cout << g;
 }
+
