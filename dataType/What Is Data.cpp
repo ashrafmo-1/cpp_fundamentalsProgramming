08853eb76af12a91e4e1a8ce92a0bf474e414cb6
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-03-06 15:29:15 +0200

    start data type

---
+++ dataType/What Is Data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str = "ashraf mohamed";
+  int num = 9999;
+  bool done = true;
+
+  // get variable size
+  cout << sizeof(str) << "\n";
+  cout << sizeof(num) << "\n";
+  cout << sizeof(done) << "\n";
+
+  return 0;
+}
